be3970d0b60e119eef6cca66aa6fd09b7acddcb6
Daniel Tull <dt@danieltull.co.uk>
2011-02-23 22:30:13 +0000

    Initial commit

---
+++ DataCenter Project/DataCenter/DataCenterAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  DataCenterAppDelegate.h
+//  DataCenter
+//
+//  Created by Daniel Tull on 23.02.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataCenterAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
