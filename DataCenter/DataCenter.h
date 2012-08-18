251a7b7ca982275ac16ff2e8838821b712850ec1
Daniel Tull <dt@danieltull.co.uk>
2012-08-18 15:39:14 +0100

    Create new project

---
+++ DataCenter/DataCenter.h
@@ -0,0 +1,13 @@
+//
+//  DataCenter.h
+//  DataCenter
+//
+//  Created by Daniel Tull on 18.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataCenter : NSObject
+
+@end
---
+++ DataCenter/DataCenter.m
@@ -0,0 +1,13 @@
+//
+//  DataCenter.m
+//  DataCenter
+//
+//  Created by Daniel Tull on 18.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "DataCenter.h"
+
+@implementation DataCenter
+
+@end
