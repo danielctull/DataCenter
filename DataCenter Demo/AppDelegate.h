bbfca90267392112d8071955a7382a1e0796b82e
Daniel Tull <dt@danieltull.co.uk>
2012-08-18 16:04:33 +0100

    Add new demo app

---
+++ DataCenter Demo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DataCenter Demo
+//
+//  Created by Daniel Tull on 18.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DataCenter Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DataCenter Demo
+//
+//  Created by Daniel Tull on 18.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
